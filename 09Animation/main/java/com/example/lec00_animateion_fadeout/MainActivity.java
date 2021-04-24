aa4dda0365bd5276fce8924b3ee57d6dfed417c4
michjung <jung8508@gmail.com>
2021-04-24 19:34:26 +0900

    Animation _ fadout with tranform axis and rotation

---
+++ 09Animation/main/java/com/example/lec00_animateion_fadeout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lec00_animateion_fadeout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
