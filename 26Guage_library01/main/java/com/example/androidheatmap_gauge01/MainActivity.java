55cfa078113651d8e03c4745a25bec87f1a03a43
michjung <jung8508@gmail.com>
2021-06-01 03:34:48 +0900

    Gauge_Library01_Not completed

---
+++ 26Guage_library01/main/java/com/example/androidheatmap_gauge01/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.androidheatmap_gauge01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Locale;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import pl.pawelkleczkowski.customgauge.CustomGauge;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomGauge gauge1;
+    private CustomGauge gauge2;
+    private CustomGauge gauge3;
+
+    int i;
+    private TextView text1;
+    private TextView text2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(toolbar);
+
+        Button button = findViewById(R.id.button);
+        gauge1 = findViewById(R.id.gauge1);
+        gauge2 = findViewById(R.id.gauge2);
+        gauge3 = findViewById(R.id.gauge3);
+
+        text1  = findViewById(R.id.textView1);
+        text2  = findViewById(R.id.textView2);
+        text1.setText(String.valueOf(gauge1.getValue()));
+        text2.setText(String.valueOf(gauge2.getValue()));
+        text2.setText(String.valueOf(gauge2.getValue()));
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                gauge2.setEndValue(800);
+                gauge2.setValue(200);
+                text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge2.getValue(), gauge2.getEndValue()));
+                new Thread() {
+                    public void run() {
+                        for (i=0;i<100;i++) {
+                            if (i == 50) {
+                                gauge2.setEndValue(1200);
+                            }
+                            try {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        gauge1.setValue(i*10);
+                                        gauge2.setValue(200 + i*5);
+                                        gauge3.setValue(i);
+                                        text1.setText(String.valueOf(gauge1.getValue()));
+                                        text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge2.getValue(), gauge2.getEndValue()));
+                                    }
+                                });
+                                Thread.sleep(50);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }.start();
+            }
+        });
+    }
+
+}
